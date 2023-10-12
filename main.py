078a125248ad1479807b012313571ca025ff716b
JonaikerJaspe <JonaikerJaspe@gmail.com>
2023-10-12 11:04:02 -0400

    request_response_body

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
 from fastapi import FastAPI
+from fastapi import Body
+
+
 
 app = FastAPI()
+
+# Models
+
+class Person (BaseModel):
+    firstName: str
+    lastName:str
+    age:int
+    hairColor:Optional[str] = None
+    isMarried: Optional[bool] = None
+
 
 @app.get("/")
 def home():
     return {"Hello":"world"}
+
+
+
+@app.post("/person/new")
+def createPerson( person:Person = Body(...)):
+    return person
